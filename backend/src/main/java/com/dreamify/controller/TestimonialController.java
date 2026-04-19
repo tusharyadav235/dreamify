40f1d3a326d9368805d445ddb9ac11ad63e8da84
tusharyadav235 <tusharforwork00@gmail.com>
2026-04-19 22:27:23 +0530

    Update TestimonialController.java

--- backend/src/main/java/com/dreamify/controller/TestimonialController.java
+++ backend/src/main/java/com/dreamify/controller/TestimonialController.java
@@ -11,7 +11,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/testimonials")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*")
 public class TestimonialController {
 
     private final TestimonialRepository repo;
